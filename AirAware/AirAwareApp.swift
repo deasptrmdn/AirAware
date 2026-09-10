c8822f80388d669bc258cbd002a3c7132c96a49b
deas <deaspfr1@gmail.com>
2026-09-10 14:22:01 +0700

    Initial Commit

---
+++ AirAware/AirAwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirAwareApp.swift
+//  AirAware
+//
+//  Created by user on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct AirAwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
